e2ca6bf3f5e06a47247ea04a13e6bb2af24a0e76
AbidHasan007 <ahmasum890@gmail.com>
2024-02-19 13:01:42 +0600

    added price mechanism

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 
 
 
-const seats = document.querySelectorAll('.kbd')
+const seats = document.querySelectorAll('.seat')
 
 
- 
- for(let index = 1; index < seats.length; index++)
+
+let seatCount = ""; 
+let count = 0;
+
+ for(let index = 0; index < seats.length; index++)
  {
     const seat = seats[index];
-    seat.addEventListener('click', function(){
+    seat.addEventListener('click', function(e){
+       console.log(e.target.innerText)
+       if(seatCount === e.target.innerText ){
+        alert('Do you want to buy 1 ticket 2 times? But You cannt...');
+        return ;
+       }else if(parseInt(document.getElementById('selected-seat-count').innerText) >= 4){
+        alert('Sorry! You can buy only 4 tickets');
+        return ;
+       }
         seat.classList.add('bg-[#1DD100]')
         const currentSeatElement = document.getElementById('selected-seat-count');
          const seatValue = currentSeatElement.innerText;
@@ -24,6 +35,26 @@ const seats = document.querySelectorAll('.kbd')
           const currentTotal = parseInt(totalSeatvalue);
           const seatLeft = currentTotal-1;
           totalSeat.innerText = seatLeft;
+
+          const seatNameElement = e.target.innerText;
+          seatCount = seatNameElement;
+          document.getElementById('dynamic-container').innerHTML += `
+          <div class="flex items-center justify-between">
+                              <p id="seat-class "> ${seatNameElement}</p>
+                              <p >Economy</p>
+                              <p id="price">500</p>
+                            </div>
+          `;
+
+          const ticketSelected = parseInt(newSeat);
+          const total = 500*ticketSelected;
+          document.getElementById('total').innerText = total;
+
          
      })
  }
+
+function cupon(){
+
+  
+}
